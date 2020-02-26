1c5840a44ae479cee42c8aa7d2eb60e5462961c2
tbl4h <mic.maci2891@gmail.com>
2020-02-26 23:41:58 +0100

    poprawna funkcja nazwy klas

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -1,15 +1,22 @@
 var allClasses = [];
-
+var allClassesName = [];
 var allElements = document.body.querySelectorAll('[class*=tag]');
 elementLoop:
 for(var i =0;i<allElements.length;i++){
     var potential = allElements[i];
     classLoop:
     for(var j=0;j<potential.classList.length;j++){
-        if(potential.classList[j].match(/^tag/)){
-            allClasses.push(potential); 
+        if(potential.classList[j].match(/^tag/)) {
+            let tmp = potential.classList[j];            
+            if(allClassesName.indexOf(tmp) === -1)
+                allClassesName.push(tmp);
         }
     }
 }
 
-console.log(allClasses);
+for (i = 0; i<allClasses.length;i++){
+
+}
+console.log("akl");
+console.log(allClassesName);
+console.log(allClasses.length);
